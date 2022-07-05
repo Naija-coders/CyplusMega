df439c548370e0ecfb36498ec3a84366e67b9e76
Oghenemano <oghenemano@MacBook-Pro.local>
2022-07-06 02:59:28 +0300

    adding second banner image

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -32,14 +32,21 @@ export default function Banner() {
             />
           </div>
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+
+        <SwiperSlide>
+          {" "}
+          <div style={{ width: "100%", height: "100%" }}>
+            <img
+              src="bannerservice2.png"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectPosition: "center -4px",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+        </SwiperSlide>
       </Swiper>
     </>
   );
